f3702db04d29ced6ae3c71b7f0a86b2e2c5782d9
nbchicong <nbchicong@gmail.com>
2016-09-08 23:51:45 +0700

    entry lis

---
+++ angular/app/components/entry-lists/entry-lists.component.js
@@ -0,0 +1,17 @@
+class EntryListsController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const EntryListsComponent = {
+    templateUrl: './views/app/components/entry-lists/entry-lists.component.html',
+    controller: EntryListsController,
+    controllerAs: 'vm',
+    bindings: {}
+}
